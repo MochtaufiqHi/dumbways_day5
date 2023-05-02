0f317293319518e0d1d8cbdea8ecd48fe9fdd44b
mochtaufiqidayat <mochammadtaufiq67@gmail.com>
2023-05-02 10:02:12 +0700

    update day 13 to fecth data from database

--- main.go
+++ main.go
@@ -3,43 +3,56 @@ package main
 import (
 	// "echo.go"
 
+	"context"
 	"fmt"
+	"myportfolio/connection"
 	"net/http"
 	"strconv"
 	"text/template"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Project struct {
-	ProjectName string
-	Description string
-	StartDate   string
-	EndDate     string
+	Id           int
+	ProjectName  string
+	StartDate    time.Time
+	EndDate      time.Time
+	Description  string
+	Technologies []string
+	Image        string
+	Duration     int64
+	Char         string
 }
 
 var dataProject = []Project{
 	{
 		ProjectName: "Aplikasi Sederhana",
 		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		StartDate:   "13/04/2020",
-		EndDate:     "13/04/2023",
+		//StartDate:   "13/04/2020",
+		//EndDate:     "13/04/2023",
 	},
 	{
 		ProjectName: "Aplikasi Sederhana",
 		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		StartDate:   "13/04/2020",
-		EndDate:     "13/04/2023",
+		// StartDate:   "13/04/2020",
+		// EndDate:     "13/04/2023",
 	},
 	{
 		ProjectName: "Aplikasi Sederhana",
 		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		StartDate:   "13/04/2020",
-		EndDate:     "13/04/2023",
+		// StartDate:   "13/04/2020",
+		// EndDate:     "13/04/2023",
 	},
 }
 
 func main() {
+	// create connection from database
+	connection.DatabaseConnect()
+
+	// fmt.Println(dataProject)
+
 	// initialization echo
 	e := echo.New()
 
@@ -57,6 +70,7 @@ func main() {
 	e.GET("register", register)
 
 	e.Logger.Fatal(e.Start(":5000"))
+
 }
 
 func home(c echo.Context) error {
@@ -68,9 +82,71 @@ func home(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
 	}
 
-	projects := map[string]interface{}{
-		"Project": dataProject,
+	// map(data type) => key and value
+	data, _ := connection.Conn.Query(context.Background(), "SELECT id, name, start_date, end_date, description, technologies, image FROM tb_projects")
+
+	// create array for the result
+	var result []Project
+
+	for data.Next() {
+		var each = Project{}
+
+		err := data.Scan(&each.Id, &each.ProjectName, &each.StartDate, &each.EndDate, &each.Description, &each.Technologies, &each.Image)
+		if err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+		}
+		// each.Author = ""
+
+		firstDate := &each.StartDate
+		secondDate := &each.EndDate
+		difference := secondDate.Sub(*firstDate)
+
+		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24/30/12))
+		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24/30))
+		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24))
+		// fmt.Printf("Months: %d\n", int64(difference.Hours()))
+
+		d := int64(difference.Hours() / 24 / 30)
+
+		each.Duration = d
+
+		// t := each.Technologies
+		// for _, char := range t {
+		// 	switch char {
+		// 	case "reactjs":
+		// 		// p := `<img src="/public/img/nodejs.png>`
+		// 		// return p
+		// 		fmt.Println(`<img src="/public/img/reactjs.png>`)
+		// 	case "nodejs":
+		// 		fmt.Println(`<img src="/public/img/nodejs.png>`)
+		// 	case "nextjs":
+		// 		fmt.Println(`<img src="/public/img/nextJs.png>`)
+		// 	case "typescript":
+		// 		fmt.Println(`<img src="/public/img/ts.png>`)
+		// 	default:
+		// 		fmt.Println("")
+		// 	}
+		// }
+
+		// for _, char := range t {
+		// 	if char == "reactjs" {
+		// 		return ""
+		// 	}
+		// }
+
+		result = append(result, each)
 	}
+
+	// fmt.Printf("%v", result)
+
+	projects := map[string]interface{}{
+		"Project": result,
+	}
+
+	// projects := map[string]interface{}{
+	// 	"Project": dataProject,
+	// }
 
 	return tmpl.Execute(c.Response(), projects)
 }
