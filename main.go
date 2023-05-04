5930ba6206027ea5c82d452173b3450cb770e4e5
mochtaufiqidayat <mochammadtaufiq67@gmail.com>
2023-05-04 22:01:06 +0700

    CRUD with database

--- main.go
+++ main.go
@@ -26,27 +26,6 @@ type Project struct {
 	Char         string
 }
 
-var dataProject = []Project{
-	{
-		ProjectName: "Aplikasi Sederhana",
-		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		//StartDate:   "13/04/2020",
-		//EndDate:     "13/04/2023",
-	},
-	{
-		ProjectName: "Aplikasi Sederhana",
-		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		// StartDate:   "13/04/2020",
-		// EndDate:     "13/04/2023",
-	},
-	{
-		ProjectName: "Aplikasi Sederhana",
-		Description: "Aplikasi sederhana yang dibangun menggunakan Javascript dan Boostraps",
-		// StartDate:   "13/04/2020",
-		// EndDate:     "13/04/2023",
-	},
-}
-
 func main() {
 	// create connection from database
 	connection.DatabaseConnect()
@@ -66,8 +45,10 @@ func main() {
 	e.GET("/detail-project/:id", detail)
 	e.POST("/add-project", addProject)
 	e.GET("/delete/:id", deleteProject)
-	e.GET("login", login)
-	e.GET("register", register)
+	e.GET("/login", login)
+	e.GET("/register", register)
+
+	e.GET("/update/:id", updateProject)
 
 	e.Logger.Fatal(e.Start(":5000"))
 
@@ -77,8 +58,6 @@ func home(c echo.Context) error {
 	tmpl, err := template.ParseFiles("views/index.html")
 
 	if err != nil {
-		// fmt.Println("Page not found")
-		// return c.String(http.StatusOK, "Hello World!!")
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
 	}
 
@@ -96,57 +75,21 @@ func home(c echo.Context) error {
 			fmt.Println(err.Error())
 			return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 		}
-		// each.Author = ""
 
 		firstDate := &each.StartDate
 		secondDate := &each.EndDate
 		difference := secondDate.Sub(*firstDate)
 
-		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24/30/12))
-		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24/30))
-		// fmt.Printf("Months: %d\n", int64(difference.Hours()/24))
-		// fmt.Printf("Months: %d\n", int64(difference.Hours()))
-
 		d := int64(difference.Hours() / 24 / 30)
 
 		each.Duration = d
 
-		// t := each.Technologies
-		// for _, char := range t {
-		// 	switch char {
-		// 	case "reactjs":
-		// 		// p := `<img src="/public/img/nodejs.png>`
-		// 		// return p
-		// 		fmt.Println(`<img src="/public/img/reactjs.png>`)
-		// 	case "nodejs":
-		// 		fmt.Println(`<img src="/public/img/nodejs.png>`)
-		// 	case "nextjs":
-		// 		fmt.Println(`<img src="/public/img/nextJs.png>`)
-		// 	case "typescript":
-		// 		fmt.Println(`<img src="/public/img/ts.png>`)
-		// 	default:
-		// 		fmt.Println("")
-		// 	}
-		// }
-
-		// for _, char := range t {
-		// 	if char == "reactjs" {
-		// 		return ""
-		// 	}
-		// }
-
 		result = append(result, each)
 	}
-
-	// fmt.Printf("%v", result)
 
 	projects := map[string]interface{}{
 		"Project": result,
 	}
-
-	// projects := map[string]interface{}{
-	// 	"Project": dataProject,
-	// }
 
 	return tmpl.Execute(c.Response(), projects)
 }
@@ -155,7 +98,6 @@ func contact(c echo.Context) error {
 	tmpl, err := template.ParseFiles("views/contact_form.html")
 
 	if err != nil {
-		// fmt.Println("Page not found")
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
 	}
 
@@ -166,7 +108,6 @@ func project(c echo.Context) error {
 	tmpl, err := template.ParseFiles("views/my_project.html")
 
 	if err != nil {
-		// fmt.Println("Page not found")
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
 	}
 
@@ -182,28 +123,62 @@ func detail(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
 	}
 
+	var ProjectDetail = Project{}
+
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, name, start_date, end_date, description, technologies, image  FROM tb_projects WHERE id = $1", id).Scan(&ProjectDetail.Id, &ProjectDetail.ProjectName, &ProjectDetail.StartDate, &ProjectDetail.EndDate, &ProjectDetail.Description, &ProjectDetail.Technologies, &ProjectDetail.Image)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
+	}
+
 	data := map[string]interface{}{
-		"Id":          id,
-		"ProjectName": "Aplikasi sederhana yang dibangun oleh golang",
-		"Description": "saya merasakn apa yang anda rasakan sekarang",
+		"Project": ProjectDetail,
 	}
 
 	return tmpl.Execute(c.Response(), data)
 }
 
 func addProject(c echo.Context) error {
+	c.Request().ParseForm()
+
 	projectName := c.FormValue("projectName")
+	startDate := c.FormValue("start-date")
+	endDate := c.FormValue("end-date")
+	nodeJs := c.FormValue("nodeJs")
+	reactJs := c.FormValue("reactJs")
+	nextJs := c.FormValue("nextJs")
+	typescript := c.FormValue("typescript")
 	description := c.FormValue("description")
+	image := "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
 
-	var addProject = Project{
-		ProjectName: projectName,
-		Description: description,
+	var technologies []string
+
+	if nodeJs == "nodejs" {
+		technologies = append(technologies, "nodejs")
+	} else {
+		technologies = append(technologies, "")
 	}
-	// fmt.Println(projectName)
-	// fmt.Println(description)
+	if reactJs == "reactjs" {
+		technologies = append(technologies, "reactjs")
+	} else {
+		technologies = append(technologies, "")
+	}
+	if nextJs == "nextjs" {
+		technologies = append(technologies, "nextjs")
+	} else {
+		technologies = append(technologies, "")
+	}
+	if typescript == "typescript" {
+		technologies = append(technologies, "typescript")
+	} else {
+		technologies = append(technologies, "")
+	}
 
-	fmt.Println(addProject)
-	dataProject = append(dataProject, addProject)
+	_, err := connection.Conn.Exec(context.Background(), "INSERT INTO public.tb_projects(name, start_date, end_date, description, technologies, image) VALUES($1, $2, $3, $4, $5, $6)", projectName, startDate, endDate, description, technologies, image)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
+	}
 
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
@@ -211,9 +186,62 @@ func addProject(c echo.Context) error {
 func deleteProject(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	dataProject = append(dataProject[:id], dataProject[id+1:]...)
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_projects WHERE id=$1", id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
+	}
 
 	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+
+func updateProject(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	c.Request().ParseForm()
+
+	projectName := c.FormValue("projectName")
+	// startDate := c.FormValue("start-date")
+	// endDate := c.FormValue("end-date")
+	nodeJs := c.FormValue("nodeJs")
+	reactJs := c.FormValue("reactJs")
+	nextJs := c.FormValue("nextJs")
+	typescript := c.FormValue("typescript")
+	description := c.FormValue("description")
+	image := "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+	var technologies []string
+
+	if nodeJs == "nodejs" {
+		technologies = append(technologies, "nodejs")
+	} else {
+		technologies = append(technologies, "")
+	}
+	if reactJs == "reactjs" {
+		technologies = append(technologies, "reactjs")
+	} else {
+		technologies = append(technologies, "")
+	}
+	if nextJs == "nextjs" {
+		technologies = append(technologies, "nextjs")
+	} else {
+		technologies = append(technologies, "")
+	}
+	if typescript == "typescript" {
+		technologies = append(technologies, "typescript")
+	} else {
+		technologies = append(technologies, "")
+	}
+
+	// _, err := connection.Conn.Exec(context.Background(), "UPDATE tb_projects SET name=$1, start_date=$2, end_date=$3, description=$4, technologies=$5, image=$6", projectName, startDate, endDate, description,
+	// technologies, image)
+
+	_, err := connection.Conn.Exec(context.Background(), "UPDATE tb_projects SET name=$1, description=$2, technologies=$3, image=$4 WHERE id=$5", projectName, description, technologies, image, id)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message ": err.Error()})
+	}
+
+	return c.Redirect(http.StatusMovedPermanently, "/project")
 }
 
 func login(c echo.Context) error {
